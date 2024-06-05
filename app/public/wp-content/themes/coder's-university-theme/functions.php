4141fb8201341cf44410766056ae76262ac4bdca
amannpoddar <amankumar9108@gmail.com>
2024-06-05 18:10:42 +0530

    created new custom fields for professor banner page

--- app/public/wp-content/themes/coder's-university-theme/functions.php
+++ app/public/wp-content/themes/coder's-university-theme/functions.php
@@ -1,4 +1,8 @@
 <?php
+
+function pageBanner(){
+    
+}
 function university_files()
 {
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
@@ -15,6 +19,7 @@ function university_features()
     add_theme_support('post-thumbnails');
     add_image_size('professorLandscape', 400, 260, true);
     add_image_size('professorPortrait', 480, 650, true);
+    add_image_size('pageBanner', 1500, 350, true);
 }
 add_action('after_setup_theme', 'university_features');
 
